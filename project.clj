be499f02f064a6ff279857dfdbb8fc5c7e029b18
Andrew Rosa <andrewhr@me.com>
2017-01-31 19:52:35 -0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.com/andrewhr/codeclimate-kibit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.cli "0.3.3"]
-                 [jonase/kibit "0.1.2"]
-                 [cheshire "5.5.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [jonase/kibit "0.1.3"]
+                 [cheshire "5.7.0"]]
   :aot :all
   :main codeclimate.kibit
   :uberjar-name "codeclimate-kibit.jar")
