6ece3dade85d186862f6b6183b20f547a0e73cdd
Łukasz Korecki <lukasz@korecki.me>
2018-08-17 09:14:38 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.com/andrewhr/codeclimate-kibit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.cli "0.3.3"]
-                 [jonase/kibit "0.1.2"]
-                 [cheshire "5.5.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.cli "0.3.7"]
+                 [jonase/kibit "0.1.6"]
+                 [cheshire "5.8.0"]]
   :aot :all
   :main codeclimate.kibit
   :uberjar-name "codeclimate-kibit.jar")
